2e0ce67bb13a716999a1479f2d0267d5dcd9d86e
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-05-01 11:46:42 -0700

    filled in the dto's

--- Models/DTO/LoginDTO.cs
+++ Models/DTO/LoginDTO.cs
@@ -7,6 +7,7 @@ namespace CodeWarsBackend.Models.DTO
 {
     public class LoginDTO
     {
-        
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
