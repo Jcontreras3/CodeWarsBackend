562427848aa4d45fcd7b4eb869871182265ed3b6
Jovann Contreras <jcontreras@codestack.co>
2023-05-01 11:44:37 -0700

    added packages and some endpoint to controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CodeWarsBackend.Models.DTO;
+using CodeWarsBackend.Models;
+using CodeWarsBackend.Services;
 
 namespace CodeWarsBackend.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly UserService _data;
+
+        public UserController(UserService dataFromService){
+            _data = dataFromService;
+        }
+
+        // [HttpPost]
+        // [Route("Login")]
+        // public IActionResult Login(LoginDTO User){
+        //     return _data.Login(User);
+        // }
         
     }
 }
--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeWarsBackend.Models;
 
 namespace CodeWarsBackend.Services.Context
 {
     public class DataContext
     {
-        
+        // public DbSet<UserModel> UserInfo { get; set; }
+
+
+
+        // public DataContext(DbContextOptions options) : base(options)
+        // { }
+
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     base.OnModelCreating(builder);
+        // }
+
     }
 }
